2cfd7eb4e0f2c0f942f1ad1b89d5eabd07516b43
Devin Termini <33554022+devjt2@users.noreply.github.com>
2024-03-11 16:48:33 -0700

    Instantiate new 'VantageWorkflowJobInputs' correctly.

--- VantageLibrary/Types/ResponseTypes/VantageWorkflowJobInputs.cs
+++ VantageLibrary/Types/ResponseTypes/VantageWorkflowJobInputs.cs
@@ -1,9 +1,9 @@
 ﻿namespace VantageLibrary.Types; 
 public class VantageWorkflowJobInputs {
-    public List<VantageAttachment> Attachments { get; set; }
-    public string JobName { get; set; }
-    public List<VantageLabel> Labels { get; set; }
-    public List<VantageMedia> Medias { get; set; }
-    public int Priority { get; set; }
-    public List<VantageVariable> Variables { get; set; }
+    public List<VantageAttachment> Attachments { get; set; } = new List<VantageAttachment>();
+    public string JobName { get; set; } = "Default Job Name";
+    public List<VantageLabel> Labels { get; set; } = new List<VantageLabel>();
+    public List<VantageMedia> Medias { get; set; } = new List<VantageMedia>();
+    public int Priority { get; set; } = 0;
+    public List<VantageVariable> Variables { get; set; } = new List<VantageVariable> { };
 }
